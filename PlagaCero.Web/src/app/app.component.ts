f7480eb424a6ddae5284b804994962ed6599157b
emiduran05 <emiduranf05@gmail.com>
2024-11-11 23:30:43 -0600

    User Interface
    cree la interfaz que verá el usuario cuando acceda al estado de sus plantas

--- PlagaCero.Web/src/app/app.component.ts
+++ PlagaCero.Web/src/app/app.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './site_header/header/header.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent ],
+  imports: [RouterOutlet, HeaderComponent, NgFor, NgIf ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'PlagaCero.Web';
+
+  plantas:any = [
+
+    {
+      color: "verde",
+      temperatura: 32,
+      humedad: 20,
+      estado: "sano",
+    },
+
+
+    {
+      color: "amarillo",
+      temperatura: 40,
+      humedad: 10,
+      estado: "infectado",
+    },
+
+
+    {
+      color: "amarillo",
+      temperatura: 35,
+      humedad: 18,
+      estado: "riesgo",
+    },
+  ]
+
+  
+
 }
